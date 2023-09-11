cfb39e16683cb8f16d1401f3b0e813cc686ee1a2
peakfiction <sakhran.t@gmail.com>
2023-09-11 16:24:59 +0700

    i forgor lmao

--- main/views.py
+++ main/views.py
@@ -4,7 +4,11 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'name': 'Muhammad Sakhran',
-        'class': 'PBP KI'
+        'class': 'PBP KI',
+        'description': "From the innovative minds at Vault-Tec, we proudly present the Pip-Boy Inventory Manager, a cutting-edge web-based solution designed to enhance your post-apocalyptic survival experience. Just like its wrist-mounted predecessor, the Pip-Boy Inventory Manager offers seamless access to your crucial inventory data. Seamlessly navigate your stash of equipment, manage your consumables, and keep tabs on your quest progress, all from the comfort of your preferred device's web browser. Vault-Tec's commitment to user-friendly design ensures that you can effortlessly organize your post-apocalyptic life, whether it's keeping track of your precious bottle caps or maintaining your arsenal of weaponry. Surviving the wasteland has never been this organized, thanks to the Pip-Boy Inventory Manager—a technological marvel for the modern survivor.",
+        'version': "Pip-Boy 3000 Mark IV Web Version",
+        'releasedate': "2287"
+        
     }
 
     return render(request, 'main.html', context)
